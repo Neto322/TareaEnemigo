410cd91a8bafa17cce73e5a790a5625e96b2cfb1
Neto322 <42181276+Neto322@users.noreply.github.com>
2018-08-16 00:19:38 -0500

    Add files via upload

---
+++ TareaEnemigo/TareaEnemigo/Enemigo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaEnemigo
+{
+    class Enemigo
+    {
+        // auto properties 
+        public string NombreEnemigo { get; set; }
+        public int PosicionX { get; set; }
+        public int PosicionY { get; set; }
+        public int Puntos { get; set; }
+
+        //Properties
+        private int _puntos;
+        public int puntos
+        {
+            get {
+                return _puntos;
+            }
+            set {
+                if (value > 100)
+                {
+                    _puntos = value;
+
+                }
+                else
+                {
+                    Console. WriteLine("Puntos insuficientes");
+                }
+                
+               
+            }
+        }
+    }
+}
---
+++ TareaEnemigo/TareaEnemigo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaEnemigo
+{
+    class Enemigo
+    {
+        static void Main(string[] args)
+        {
+            // We created a contact instance
+            Enemigo enemigouno = new Enemigo();
+            enemigouno.NombreEnemigo = "Mauricio";
+            enemigouno.PosicionX = "32";
+            enemigouno.PosicionY = "8";
+            enemigouno.Puntos = "230";
+            
+
+            Console.WriteLine("Nombre:" + enemigouno.NombreEnemigo);
+
+            Console.WriteLine("Posicion X:" + enemigouno.PosicionX);
+
+            Console.WriteLine("Posicion Y:" + enemigouno.PosicionY);
+
+            Console.WriteLine("Puntos:" + enemigouno.Puntos);
+
+           
+
+            Console.ReadLine();
+            
+        }
+    }
+}
